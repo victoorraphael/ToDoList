9102b86d56d858654524ac3639771f0805bbc8fe
victoorraphael <raphaelvictor27@gmail.com>
2020-06-18 10:37:35 -0300

    add EJS

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  const day = ["Monday", "Sunday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const dayOfWeek = day[currentDay];
+
+  res.render("list", {dayOfWeek: dayOfWeek});
+
+})
+
+
+app.listen(3000, console.log("Server is running on port 3000!"))
