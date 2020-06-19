98e8716de65fc842346851d2bb30d590b6457d2a
victoorraphael <raphaelvictor27@gmail.com>
2020-06-19 14:28:14 -0300

    add items functionality dynamic

--- app.js
+++ app.js
@@ -3,17 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); // static files like css, some js ...
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  const day = ["Monday", "Sunday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  const dayOfWeek = day[currentDay];
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }
+  let currentDay = today.toLocaleDateString("en-GB", options);
 
-  res.render("list", {dayOfWeek: dayOfWeek});
+  res.render("list", {dayOfWeek: currentDay, newListItems: items});
 
+})
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 })
 
 
